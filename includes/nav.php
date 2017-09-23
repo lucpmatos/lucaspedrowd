48d602615fa0de5ee78d1f92259fa548080574c3
lucpmatos <lucpmatos@gmail.com>
2017-09-23 03:05:32 -0300

    lpwd_v1.0.0.0

---
+++ includes/nav.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="topo">
+      <div class="logo">
+        <img src="img/logo_lp_webdesigner.png" alt="Lucas Pedro Web Designer" height="50">
+      </div>
+      <a href="#">Menu<i class="fa fa-bars"></i></a>
+    </div><!-- topo -->
+    <div class="menu">
+      <div class="topo-menu"><a href="#">Fechar<i class="fa fa-remove"></i></a></div>
+      <div class="menu-itens">
+        <a href="#">Início</a>
+        <a href="#">Sobre</a>
+        <a href="#">Serviços</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Conhecimentos Técnicos</a>
+        <a href="#">Contato</a>        
+      </div>
+    </div><!-- menu -->
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <title>Lucas Pedro Web Designer</title>
+  <link rel="icon" href="img/icon_lp_webdesigner.png" type="image/x-icon" />
+  <!-- ESTILO -->
+  <link rel="stylesheet" type="text/less" href="css/main.less">
+  <!-- FONT AWESOME -->
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+  <!-- LESS -->
+  <script type="text/javascript" src="less/dist/less.js"></script>
+</head>
+<body>
+  <?php include_once "includes/nav.php" ?>
+</body>
+</html>
